e7da154f8e6529634002dd315f854a460dab86d1
Zack <zack@zacharymaril.com>
2024-08-28 05:17:56 -0400

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# Hancock 
+![John Hancock's Signature](./hancock_signature.png)
+
+A tool for finding and collating all of the function signatures in a git repository, particularly for the linux kernel.
+
+
+## Usage
+
+```bash
+git clone https://github.com/zackmaril/hancock.git
+cd hancock
+git clone https://github.com/torvalds/linux.git artifacts/linux
+poetry install
+poetry run python hancock/main.py --repo artifacts/linux --output blobs/linux_functions.json
+gzip blobs/linux_functions.json
+# should be about 5.7mb gzip, 80mb unzipped.
+```
+
+## Motivation
+
+[bpfquery.com](https://bpfquery.com) takes in sql, compiles it to bpftrace code and then runs and visualizes the results in real time. bpftrace lets you hook into the kprobes, which correspond to various linux kernel routines. While linux will tell you what kprobes are available on a specific instance, it doesn't tell you what the function arguments are nor the return type. This has meant that one has to do a bunch of CAST's when writing bpftrace programs in order to work with the pointers to various types, and it also makes it difficult to do typeahead completion with editors. So, I wrote this tool to crawl the linux kernel source and extract the function signatures from the code. 
+
+# Design 
+
+In flux. Right now it can dump out all the function signatures for a particular tagged release of the linux kernel into json. Next up, I want to be able to produce  a json blob for all tagged releases and produce a unified index. The goal of bpfquery is to let you do adhoc queries on any linux kernel release, so I want to have a single json blob that contains all the function signatures for all releases and see how big that gets.
+
+## License
+
+MIT
+
---
+++ hancock/main.py
@@ -0,0 +1,124 @@
+#Use gitpython to read the tags of artifact/linux
+
+import os
+import sys
+import json
+import re
+import subprocess
+import multiprocessing
+import argparse
+from functools import partial
+
+def get_indexed_tags(repo_path):
+    # run tags command
+    tags = subprocess.check_output(['git', 'tag'], cwd=repo_path)
+    # Split the tags by newline
+    tags = tags.decode('utf-8').split('\n')
+    # Remove the last newline
+    tags = tags[:-1]
+    # Return the tags
+    #filter out all rcx tags
+    tags = [tag for tag in tags if re.match(r'v[0-9]+\.[0-9]+$', tag)]
+
+    indexed_tags = []
+    for tag in tags:
+        # strip the v and split into major and minor
+        major, minor = tag[1:].split('.')
+        indexed = int(major) * 100 + int(minor)
+        indexed_tags.append((indexed, tag))
+        
+    indexed_tags.sort()
+    return indexed_tags 
+    
+
+def extract_functions_from_file(repo_path, file_path):
+    fp = os.path.join(repo_path, file_path)
+    # Use ctags with the specified options to extract function prototypes
+    cmd = [
+        'ctags', '--output-format=json', '--fields=+Stn', '--kinds-C=+p', '--c-types=fp', '--extras=+q', '-o', '-',
+        fp
+    ]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, text=True)
+    
+    # get the short file_path 
+    short_path = os.path.relpath(file_path, repo_path)
+    functions = []
+    for line in result.stdout.splitlines():
+        tag = json.loads(line)
+        if tag.get("name").lower() != tag.get("name"):
+            continue
+        
+        if tag.get('_type') == 'tag' and tag.get('kind') in ['function', 'prototype']:
+            function_name = tag.get('name')
+            line_number = int(tag.get('line'))
+            signature = tag.get('signature', '')
+            if signature != '':
+                #remove parens 
+                signature = signature[1:-1]
+                signature = signature.split(",")
+            typeref = tag.get('typeref', '')
+            typeref = typeref.split(':')[1] if typeref else ''
+            functions.append((short_path, function_name, line_number, typeref, signature))
+    
+    return functions
+
+
+def process_files(files, repo_path):
+    with multiprocessing.Pool() as pool:
+        functions = pool.map(partial(extract_functions_from_file, repo_path), files)
+        functions = [f for sublist in functions for f in sublist]
+    return functions
+
+def get_files_for_tag(tag, repo_path):
+    files = subprocess.check_output(['git', 'ls-tree', '-r', '--name-only', tag], cwd=repo_path)
+    files = files.decode('utf-8').split('\n')
+    files = files[:-1]
+    # only .c and .h files
+    files = [file for file in files if file.endswith('.c') or file.endswith('.h')]
+    return files
+
+def get_files_for_diff(tag1, tag2, repo_path):
+    files = subprocess.check_output(['git', 'diff', '--name-only', f'{tag1}..{tag2}'], cwd=repo_path)
+    files = files.decode('utf-8').split('\n')
+    files = files[:-1]
+    # only .c and .h files
+    files = [file for file in files if file.endswith('.c') or file.endswith('.h')]
+    return files
+
+def main():
+    # Get command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--repo', type=str, default='artifacts/linux', help='Path to the repository')
+    parser.add_argument('--output', type=str, default='blobs/linux_functions.json', help='Path to the output file')
+    args = parser.parse_args()
+
+    # Get the tags
+    tags = get_indexed_tags(args.repo)
+
+    #Checkout the first tag
+    output = subprocess.run(['git', 'checkout', '-f', tags[0][1]], cwd=args.repo)
+
+    print(output)
+
+    # Get the functions all the files with the first tag 
+    files = get_files_for_tag(tags[0][1], args.repo)
+    print(f"Found {len(files)} files in {tags[0][1]}")
+
+    # parallel extraction of functions from all files
+    functions = process_files(files, args.repo)
+    print(f"Found {len(functions)} functions in {len(files)} files")
+    # saved the functions to a file
+    with open(args.output, 'w') as f:
+        json.dump(functions, f, indent=2)
+
+    # get list of files that changed for each tag 
+    for i in range(1, len(tags)):
+        tag1 = tags[i-1][1]
+        tag2 = tags[i][1]
+        files = get_files_for_diff(tag1, tag2, args.repo)
+        print(f"Comparing {tag1} to {tag2}, {len(files)} files changed")
+    
+
+
+if __name__ == '__main__':
+    main()
